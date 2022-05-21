d0620ab3483072d6cac5dbe900b8238cdc90fe68
Varsha <varsha@Varshas-MacBook-Pro.local>
2022-05-21 00:04:38 -0700

    JS7

---
+++ html-basics/js-basics/js-exe7.js
@@ -0,0 +1,15 @@
+var a 
+var b
+var c
+
+function kgtolb(a){
+    b = a * 2.2046;
+    c = a /2.2046;
+    console.log(a+ ' is converted from kg to pound: ' +b)
+     
+    console.log(a+ ' is converted from pound to kg: ' +c)
+
+    return 
+}
+
+kgtolb(20);
