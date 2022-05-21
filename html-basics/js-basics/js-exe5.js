73e764964c8e7f075b7838acfd86f5939561b959
Varsha <varsha@Varshas-MacBook-Pro.local>
2022-05-20 23:58:06 -0700

    JS5

---
+++ html-basics/js-basics/js-exe5.js
@@ -0,0 +1,10 @@
+var a = 15
+
+function inctofeet(a){
+   console.log('To convert from inches to feet: ' + a)
+    return a * 0.083333
+}
+console.log('Converted from inches to feet: ' + inctofeet(a))
+
+
+
