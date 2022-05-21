7cad00238644e8541633f09e3d991491101ecad0
Varsha <varsha@Varshas-MacBook-Pro.local>
2022-05-20 23:34:37 -0700

    JS1

---
+++ html-basics/js-basics/js-exe1.js
@@ -0,0 +1,9 @@
+var a = 'character new length will be calculated'
+var b
+
+function firstfun() {   //function 
+   console.log(a)
+    return b= a.length   //     To calculate number of character
+}
+
+console.log(firstfun(a))    // print the final output
