b40f7309700bb41e52d6aaac9e5daa87950c2935
Varsha <varsha@Varshas-MacBook-Pro.local>
2022-05-20 23:45:03 -0700

    JS2

---
+++ html-basics/js-basics/js-exe2.js
@@ -0,0 +1,15 @@
+var a=102
+var b=20
+
+function highnumb(){
+    console.log('To find the highest number:\n' + a, + b)
+    if (a>b) 
+    { 
+        return a;
+    }
+        else  {
+            return b;
+        }
+}
+
+console.log('Highest number is: ' + highnumb())
