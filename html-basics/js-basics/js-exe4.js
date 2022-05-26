6ad1adffa6e18d28ca0fdf37d3288362a8acd580
Varsha <varsha@Varshas-MacBook-Pro.local>
2022-05-26 16:31:46 -0700

    JS 4

--- html-basics/js-basics/js-exe4.js
+++ html-basics/js-basics/js-exe4.js
@@ -1,7 +1,7 @@
-var a = 20
+//Function that accepts a number and returns the cube root of the number
 
-function numbcube(a){
+function numbcube(a) {
     console.log('To find the cube root of the given number: ' + a)
-    return Math.cbrt(a)
+    return Math.cbrt(a)                         
 }
-console.log('Cube root is: ' + numbcube(a))
+console.log('Cube root is: ' + numbcube(200))
