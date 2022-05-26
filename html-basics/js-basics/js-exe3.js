17821445f7c3eb0fdddfc1367684712bf5653be0
Varsha <varsha@Varshas-MacBook-Pro.local>
2022-05-26 16:30:40 -0700

    JS 3

--- html-basics/js-basics/js-exe3.js
+++ html-basics/js-basics/js-exe3.js
@@ -1,8 +1,7 @@
-var a = 6714;
+//Function that returns boolean flag if a number is between 5000 and 9999
 
-function boolflag(a){
+function boolflag(a) {
     console.log(a)
-     return (a>=5000 && a<=9999)
-     
+    return (a >= 5000 && a <= 9999)
 }
- console.log('Given number is Boolean: ' + boolflag(a))
+console.log('Given number is Boolean: ' + boolflag(6745))
