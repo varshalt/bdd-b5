9f2dd9dcf9bdd387d0f72cdc2e1f66b0cb0bec53
Varsha <varsha@Varshas-MacBook-Pro.local>
2022-05-26 16:29:51 -0700

    JS 2

--- html-basics/js-basics/js-exe2.js
+++ html-basics/js-basics/js-exe2.js
@@ -1,11 +1,13 @@
-function highnumb(a,b){
+//Function that accepts any two numbers and returns the largest number of the two
+
+function highnumb(a, b) {
     console.log('To find the highest number:\n' + a, + b)
-    if (a>b)                                                //Condition to check the highest of two numbers
-    { 
+    if (a > b)                                                //Condition to check the highest of two numbers
+    {
         return a;
     }
-        else  {
-            return b;
-        }
+    else {
+        return b;
+    }
 }
 console.log('Highest number is: ' + highnumb(10, 20))        //Passing inputs for the function   
