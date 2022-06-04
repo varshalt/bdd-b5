0b83e76e392473c28ea59c7f0e1330481b885138
Varsha <varsha@Varshas-MacBook-Pro.local>
2022-06-03 19:35:53 -0700

    JS 10

--- html-basics/js-basics/js-exe10.js
+++ html-basics/js-basics/js-exe10.js
@@ -1,13 +1,12 @@
-var a = 'Learning HTML and Java Script'
-var b = 'Learning to compare two strings'
+// Function that returns longest string if two strings are given as input parameter
 
-function comptostring(a,b) {   
-   console.log("\n This is the first string : " + a)
-   console.log("\n This is the second string : " + b)
-   if (a.length>=b.length)    
-    return a
-    else 
-    return b   
+function comptostring(a, b) {
+    console.log("\n This is the first string : " + a)
+    console.log("\n This is the second string : " + b)
+    if (a.length >= b.length)
+        return a
+    else
+        return b
 }
 
-console.log("\n Longest String : "  + comptostring(a,b))    
+console.log("\n Longest String : " + comptostring('HelloWorld', 'Hello'))    
