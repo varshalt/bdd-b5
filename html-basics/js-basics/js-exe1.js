76ae5a5bb938ca09d2a18b8359b0bc68e2742fe6
Varsha <varsha@Varshas-MacBook-Pro.local>
2022-06-03 19:38:38 -0700

    JS 1

--- html-basics/js-basics/js-exe1.js
+++ html-basics/js-basics/js-exe1.js
@@ -1,7 +1,8 @@
+// Function that counts the number of characters in a string
 
 function firstfun(x) {   //function 
-   console.log(x)
-    return  x.length   //     To calculate number of character
+    console.log(x)
+    return x.length   // To calculate number of character
 }
 
 console.log(firstfun('new program to count'))    // print the final output
