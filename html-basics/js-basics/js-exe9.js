6f1be6f708b461205411234413eda408369387af
Varsha <varsha@Varshas-MacBook-Pro.local>
2022-06-03 19:35:22 -0700

    JS 9

--- html-basics/js-basics/js-exe9.js
+++ html-basics/js-basics/js-exe9.js
@@ -1,24 +1,24 @@
 const NFL = {
-    "name" : "San Francisco 49ers",
-    "location" : "Santa Clara",
-    "stadium" : "Levi's Stadium",
-    "championships" : [1995, 1990, 1989, 1985, 1982],
-    "offence" : [
-        {"name":"Jimmy Garoppolo", "age":30, "pos":"QB"},
-        {"name" :"Trey Lance", "age":22, "pos":"QB"},
-        {"name" :"Brock Purdy", "age": 22, "pos": "QB"},
-        {"name" :"Nate Studfeld", "age":28, "pos":"QB"},
-        {"name" :"Tyrion Davis-Price", "age": 21, "pos":"RB"},
-        {"name" :"Austin Mack", "age": 24, "pos":"WR"}
+    "name": "San Francisco 49ers",
+    "location": "Santa Clara",
+    "stadium": "Levi's Stadium",
+    "championships": [1995, 1990, 1989, 1985, 1982],
+    "offence": [
+        { "name": "Jimmy Garoppolo", "age": 30, "pos": "QB" },
+        { "name": "Trey Lance", "age": 22, "pos": "QB" },
+        { "name": "Brock Purdy", "age": 22, "pos": "QB" },
+        { "name": "Nate Studfeld", "age": 28, "pos": "QB" },
+        { "name": "Tyrion Davis-Price", "age": 21, "pos": "RB" },
+        { "name": "Austin Mack", "age": 24, "pos": "WR" }
     ],
-    "defence" : [
-        {"name":"Arik Armstead", "age": 28, "pos":"DE"},
-        {"name" :"Alex Barrett", "age": 28, "pos":"DE"},
-        {"name" :"Darrion Daniels", "age": 24, "pos":"DT"},
-        {"name" :"Daniel Brunskill", "age": 28, "pos":"OT"},
-        {"name" :"Fred Warner", "age": 25, "pos":"LB"},
-        {"name" :"Lavert Hill", "age": 23, "pos":"CB"}
-     ]
+    "defence": [
+        { "name": "Arik Armstead", "age": 28, "pos": "DE" },
+        { "name": "Alex Barrett", "age": 28, "pos": "DE" },
+        { "name": "Darrion Daniels", "age": 24, "pos": "DT" },
+        { "name": "Daniel Brunskill", "age": 28, "pos": "OT" },
+        { "name": "Fred Warner", "age": 25, "pos": "LB" },
+        { "name": "Lavert Hill", "age": 23, "pos": "CB" }
+    ]
 
 }
 
